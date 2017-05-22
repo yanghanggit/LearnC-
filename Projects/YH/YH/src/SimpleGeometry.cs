6e99f2ddaa0b73eaf10cdd4302ae7d64e93f1764
yh <yanghang@egret.com>
2017-05-22 15:39:29 +0800

    对齐

--- Projects/YH/YH/src/SimpleGeometry.cs
+++ Projects/YH/YH/src/SimpleGeometry.cs
@@ -46,17 +46,17 @@ namespace YH
 		{
 			float[] vertices =
 			{
-					 0.5f,  0.5f, 0.0f,  // Top Right
-				     0.5f, -0.5f, 0.0f,  // Bottom Right
-				    -0.5f, -0.5f, 0.0f,  // Bottom Left
-				    -0.5f,  0.5f, 0.0f   // Top Left 
-				};
+				 0.5f,  0.5f, 0.0f,  // Top Right
+			     0.5f, -0.5f, 0.0f,  // Bottom Right
+			    -0.5f, -0.5f, 0.0f,  // Bottom Left
+			    -0.5f,  0.5f, 0.0f   // Top Left 
+			};
 
 			int[] indices =
 			{  // Note that we start from 0!
-				    0, 1, 3,  // First Triangle
-				    1, 2, 3   // Second Triangle
-				};
+			    0, 1, 3,  // First Triangle
+			    1, 2, 3   // Second Triangle
+			};
 
 			mVAO = GL.GenVertexArray();
 			mVBO = GL.GenBuffer();
