5f28986b56eb385f48478b246294fd7cd2b69a3d
yh <yanghang@egret.com>
2017-06-01 23:40:50 +0800

    整理代码

--- Projects/YH/YH/demo/HelloMaterials.cs
+++ Projects/YH/YH/demo/HelloMaterials.cs
@@ -21,14 +21,68 @@ namespace YH
 			mCameraController = new CameraController(mAppName, mCamera);
 
 			//
-			mLightShader = new GLProgram(@"Resources/basic_lighting.vs", @"Resources/basic_lighting.frag");
+			mLightShader = new GLProgram(@"Resources/materials.vs", @"Resources/materials.frag");
+
 			mLocLightModel = mLightShader.GetUniformLocation("model");
 			mLocLightView = mLightShader.GetUniformLocation("view");
 			mLocLightProjection = mLightShader.GetUniformLocation("projection");
-			mLocLightObjectColor = mLightShader.GetUniformLocation("objectColor");
-			mLocLightColor = mLightShader.GetUniformLocation("lightColor");
-			mLocLightPos = mLightShader.GetUniformLocation("lightPos");
+
+			mLocLightPos = mLightShader.GetUniformLocation("light.position");
 			mLocLightViewPos = mLightShader.GetUniformLocation("viewPos");
+
+			mLocLightAmbient = mLightShader.GetUniformLocation("light.ambient");
+			mLocLightDiffuse = mLightShader.GetUniformLocation("light.diffuse");
+			mLocLightSpecular = mLightShader.GetUniformLocation("light.specular");
+
+			mLocMaterialAmbient = mLightShader.GetUniformLocation("material.ambient");
+			mLocMaterialDiffuse = mLightShader.GetUniformLocation("material.diffuse");
+			mLocMaterialSpecular = mLightShader.GetUniformLocation("material.specular");
+			mLocMaterialShininess = mLightShader.GetUniformLocation("material.shininess");
+
+
+
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"),  ambientColor.x, ambientColor.y, ambientColor.z);
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"),  diffuseColor.x, diffuseColor.y, diffuseColor.z);
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 1.0f, 1.0f, 1.0f);
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"),   1.0f, 0.5f, 0.31f);
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"),   1.0f, 0.5f, 0.31f);
+
+			//glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"),  0.5f, 0.5f, 0.5f); // Specular doesn't have full effect on this object's material
+
+			//glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 32.0f);
+
+			// Create camera transformations
+			//glm::mat4 view;
+			//view = camera.GetViewMatrix();
+			//glm::mat4 projection = glm::perspective(camera.Zoom, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+			//// Get the uniform locations
+			//GLint modelLoc = glGetUniformLocation(lightingShader.Program, "model");
+			//GLint viewLoc = glGetUniformLocation(lightingShader.Program, "view");
+			//GLint projLoc = glGetUniformLocation(lightingShader.Program, "projection");
+			//// Pass the matrices to the shader
+			//glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+			//glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+			//// Draw the container (using container's vertex attributes)
+			//glBindVertexArray(containerVAO);
+			//glm::mat4 model;
+
+			//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			//glDrawArrays(GL_TRIANGLES, 0, 36);
+
+			//glBindVertexArray(0);
+
+
+
+
+
 
 			//
 			mLampShader = new GLProgram(@"Resources/lamp.vs", @"Resources/lamp.frag");
@@ -60,7 +114,7 @@ namespace YH
 
 				GL.UniformMatrix4(mLocLightProjection, false, ref projection);
 				GL.UniformMatrix4(mLocLightView, false, ref view);
-
+				/*
 				GL.Uniform3(mLocLightObjectColor, 1.0f, 0.5f, 0.31f);
 				GL.Uniform3(mLocLightColor, 1.0f, 1.0f, 1.0f);
 				GL.Uniform3(mLocLightPos, lightPos.X, lightPos.Y, lightPos.Z);
@@ -69,6 +123,7 @@ namespace YH
 				Matrix4 model = Matrix4.CreateTranslation(0, 0, 0);
 				model = Matrix4.CreateScale(0.5f) * model;
 				GL.UniformMatrix4(mLocLightModel, false, ref model);
+				*/
 
 				mCube.Draw();
 			}
@@ -96,13 +151,22 @@ namespace YH
 
 		//
 		private GLProgram mLightShader = null;
+
 		private int mLocLightModel = -1;
 		private int mLocLightView = -1;
 		private int mLocLightProjection = -1;
-		private int mLocLightObjectColor = -1;
-		private int mLocLightColor = -1;
+
 		private int mLocLightPos = -1;
 		private int mLocLightViewPos = -1;
+
+		private int mLocLightAmbient = -1;
+		private int mLocLightDiffuse = -1;
+		private int mLocLightSpecular = -1;
+
+		private int mLocMaterialAmbient = -1;
+		private int mLocMaterialDiffuse = -1;
+		private int mLocMaterialSpecular = -1;
+		private int mLocMaterialShininess = -1;
 
 		//
 		private GLProgram mLampShader = null;
