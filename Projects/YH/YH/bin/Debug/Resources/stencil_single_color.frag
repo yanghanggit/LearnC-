1c8033c6484cef2896bc16e6dff59f162f6d94c1
yh <yanghang@egret.com>
2017-06-14 21:48:19 +0800

    临时提交

--- Projects/YH/YH/bin/Debug/Resources/stencil_single_color.frag
+++ Projects/YH/YH/bin/Debug/Resources/stencil_single_color.frag
@@ -1,9 +1,10 @@
 #version 330 core
 
-//in vec2 TexCoords;
+in vec2 TexCoords;
 out vec4 outColor;
 
 void main()
 {
     outColor = vec4(0.04, 0.28, 0.26, 1.0);
+    outColor += vec4(TexCoords, 0, 1);
 }
--- Projects/YH/YH/demo/HelloStencilTesting.cs
+++ Projects/YH/YH/demo/HelloStencilTesting.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿﻿﻿﻿using System;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using OpenTK;
@@ -28,14 +28,17 @@ namespace YH
 			mCubeTexture = new GLTexture2D(@"Resources/Texture/marble.jpg");
 			mFloorTexture = new GLTexture2D(@"Resources/Texture/metal.png");
 
-            //
-            GL.Enable(EnableCap.DepthTest);
+			//
+			GL.Enable(EnableCap.DepthTest);
 			GL.DepthFunc(DepthFunction.Less);
 
             //
 			GL.Enable(EnableCap.StencilTest);
-			GL.StencilFunc(StencilFunction.Notequal, 1, 0xFFFFFFFF);
+            GL.StencilFunc(StencilFunction.Notequal, 1, 0xFFFFFFFF);
 			GL.StencilOp(StencilOp.Keep, StencilOp.Keep, StencilOp.Replace);
+   
+            //
+            GL.ClearColor(Color.Gray);
 		}
 
 		public override void Update(double dt)
@@ -46,79 +49,60 @@ namespace YH
 		public override void Draw(double dt, Window wnd)
 		{
 			GL.Viewport(0, 0, wnd.Width, wnd.Height);
-            GL.ClearColor(Color.Gray);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit | ClearBufferMask.StencilBufferBit);
            
-			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom), (float)wnd.Width / (float)wnd.Height, 0.1f, 100.0f);
+			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom),
+                                                                  (float)wnd.Width / (float)wnd.Height,
+                                                                  0.1f, 100.0f);
 			var view = mCamera.GetViewMatrix();
+			Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.0f, 0.0f);
 
-            //
-            shaderSingleColor.Use();
-			GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("projection"), false, ref projection);
-			GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("view"), false, ref view);
+            GL.StencilMask(0x00000000);
+
+			GL.StencilFunc(StencilFunction.Always, 1, 0xFFFFFFFF);
+			GL.StencilMask(0xFFFFFFFF);
+
+            GL.Enable(EnableCap.DepthTest);
 
             shader.Use();
 			GL.UniformMatrix4(shader.GetUniformLocation("projection"), false, ref projection);
 			GL.UniformMatrix4(shader.GetUniformLocation("view"), false, ref view);
 
-            Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.0f, 0.0f);
-
-			//==================================================================
-			GL.StencilMask(0x00000000);
-            GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
-            GL.BindTexture(TextureTarget.Texture2D, mFloorTexture.getTextureId());
-            mPlane.Draw();
-
-            //==================================================================
-            GL.StencilFunc(StencilFunction.Always, 1, 0xFFFFFFFF);
-            GL.StencilMask(0xFFFFFFFF);
             GL.BindTexture(TextureTarget.Texture2D, mCubeTexture.getTextureId());
 
             model = Matrix4.CreateTranslation(-1.0f, 0.0f, -1.0f);
-            //model = Matrix4.CreateScale(0.5f) * model;
             GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
             mCube.Draw();
 
-			//model = Matrix4.CreateTranslation(2.0f, 0.0f, 0.0f);
-            //model = Matrix4.CreateScale(0.5f) * model;
-			//GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
-			//mCube.Draw();
-
-
-            if (true)
+            if (false)
             {
-                //GL.Enable(EnableCap.StencilTest);
+                GL.BindTexture(TextureTarget.Texture2D, mCubeTexture.getTextureId());
+
+                GL.Disable(EnableCap.DepthTest);
                 GL.StencilFunc(StencilFunction.Notequal, 1, 0xFFFFFFFF);
 				GL.StencilMask(0x00000000);
-				GL.Disable(EnableCap.DepthTest);
-
-
-				const float scale = 1.1f;
-
+				
 				shaderSingleColor.Use();
 
-				GL.BindTexture(TextureTarget.Texture2D, mCubeTexture.getTextureId());
+                const float scale = 1.1f;
+
+				GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("projection"), false, ref projection);
+				GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("view"), false, ref view);
+
 				model = Matrix4.CreateTranslation(-1.0f, 0.0f, -1.0f);
 				model = Matrix4.CreateScale(scale) * model;
 				GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("model"), false, ref model);
 				mCube.Draw();
 
-				//model = Matrix4.CreateTranslation(2.0f, 0.0f, 0.0f);
-				//model = Matrix4.CreateScale(scale) * model;
-				//GL.UniformMatrix4(shaderSingleColor.GetUniformLocation("model"), false, ref model);
-				//mCube.Draw();
-
-                //
 				GL.StencilMask(0xFFFFFFFF);
-                //GL.Disable(EnableCap.StencilTest);
 				GL.Enable(EnableCap.DepthTest);
-                //GL.Clear(ClearBufferMask.StencilBufferBit);
             }
 		}
 
 		public override void OnKeyUp(OpenTK.Input.KeyboardKeyEventArgs e)
 		{
 			base.OnKeyUp(e);
+
 			if (e.Key == OpenTK.Input.Key.Plus)
 			{
 				
