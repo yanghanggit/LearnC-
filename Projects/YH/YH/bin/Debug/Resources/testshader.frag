884e6c8c535a30c3a87c4a83886e4c8546c86b8f
yh <yanghang@egret.com>
2017-06-17 18:31:08 +0800

    添加gl_FragCoord 测试

--- Projects/YH/YH/bin/Debug/Resources/testshader.frag
+++ Projects/YH/YH/bin/Debug/Resources/testshader.frag
@@ -2,7 +2,24 @@
 
 out vec4 color;
 
+uniform bool test_frag_coord;
+uniform float test_middle;
+
 void main()
 {
-	color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    if (test_frag_coord)
+    {
+        if(gl_FragCoord.x < test_middle)
+        {   
+            color = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+        }
+	    else
+        {
+            color = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+        }
+    }  
+    else
+    {
+        color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    }
 }
--- Projects/YH/YH/demo/HelloTriangle.cs
+++ Projects/YH/YH/demo/HelloTriangle.cs
@@ -1,4 +1,4 @@
-﻿﻿
+﻿﻿﻿
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 
@@ -9,7 +9,7 @@ namespace YH
 	{
 		public HelloTriangle() : base("HelloTriangle")
 		{
-
+            
 		}
 
 		public override void Start(Window wnd)
@@ -18,7 +18,9 @@ namespace YH
 			mProgram = new GLProgram(@"Resources/testshader.vert", @"Resources/testshader.frag");
 			mSimpleRectangle = new SimpleRectangle();
 
-            GL.Enable(EnableCap.ProgramPointSize);
+            GL.ClearColor(Color.Gray);
+            mTestPosition = wnd.Width / 2;
+            mMoveSpeed = wnd.Width / 10;
 		}
 
 		public override void Update(double dt)
@@ -29,11 +31,13 @@ namespace YH
 		public override void Draw(double dt, Window wnd)
 		{
 			GL.Viewport(0, 0, wnd.Width, wnd.Height);
-			GL.ClearColor(Color.Gray);
 			GL.Clear(ClearBufferMask.ColorBufferBit);
 
 			mProgram.Use();
             GL.Uniform1(mProgram.GetUniformLocation("point_size"), mPointSize);
+			GL.Uniform1(mProgram.GetUniformLocation("test_frag_coord"), mTestFragCoord ? 1 : 0);
+			GL.Uniform1(mProgram.GetUniformLocation("test_middle"), mTestPosition);
+
 
 			mSimpleRectangle.Draw();
 		}
@@ -45,23 +49,44 @@ namespace YH
 			if (e.Key == OpenTK.Input.Key.Plus)
 			{
                 mPointSize += 10.0f;
+                mTestPosition += mMoveSpeed;
 			}
 			else if (e.Key == OpenTK.Input.Key.Minus)
 			{
                 mPointSize -= 10.0f;
                 mPointSize = mPointSize > 20.0f ? mPointSize : 20.0f;
+
+				mTestPosition -= mMoveSpeed;
+				mTestPosition = mTestPosition > 0.0f ? mTestPosition : 0.0f;
 			}
 			else if (e.Key == OpenTK.Input.Key.C)
 			{
                 if (mSimpleRectangle != null)
                 {
                     mSimpleRectangle.mDrawPoints = !mSimpleRectangle.mDrawPoints;
+
+					if (mSimpleRectangle.mDrawPoints)
+					{
+						GL.Enable(EnableCap.ProgramPointSize);
+					}
+                    else
+                    {
+                        GL.Disable(EnableCap.ProgramPointSize);
+                    }
 			    }
+			}
+            else if (e.Key == OpenTK.Input.Key.B)
+			{
+                mTestFragCoord = !mTestFragCoord;
 			}
 		}
 
 		private GLProgram mProgram = null;
         private SimpleRectangle mSimpleRectangle = null;
         private float mPointSize = 100.0f;
+        private bool mTestFragCoord = false;
+        private float mTestPosition = 0.0f;
+        private float mMoveSpeed = 0.0f;
+
 	}
 }
