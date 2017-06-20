f53a6864b47af45f56b49f3ecf7364808382a1ba
yh <yanghang@egret.com>
2017-06-20 20:30:54 +0800

    改造Texture2D

--- Projects/YH/YH/src/GLTexture2D.cs
+++ Projects/YH/YH/src/GLTexture2D.cs
@@ -10,45 +10,60 @@ namespace YH
 {
 	public class GLTexture2D
 	{
-        public GLTexture2D(string texPath, bool wrapRepeat = true)
+        public GLTexture2D(string texPath, bool wrapRepeat = true, bool gammaCorrection = false)
 		{
-            mRepeatOrClampToEdge = wrapRepeat;
-			LoadFromPath(texPath);
+			LoadFromPath(texPath, wrapRepeat, gammaCorrection);
 		}
 
-		public void LoadFromPath(string texPath)
+		public void LoadFromPath(string texPath, bool repeatOrClampToEdge, bool gammaCorrection)
 		{
 			ImageReader loader = new ImageReader();
 			texPath = texPath.Replace(@"\\", @"/");
 			texPath = texPath.Replace(@"\", @"/");
 
-
-            mIsPNG = texPath.EndsWith(@".png");
+            bool isPng = texPath.EndsWith(@".png");
 
 			using (System.IO.Stream stream = File.Open(texPath, FileMode.Open))
 			{
-				StbSharp.Image image = loader.Read(stream, mIsPNG ? Stb.STBI_rgb_alpha : Stb.STBI_rgb);
+				StbSharp.Image image = loader.Read(stream, isPng ? Stb.STBI_rgb_alpha : Stb.STBI_rgb);
 				mTextureId = GL.GenTexture();
 				GL.BindTexture(TextureTarget.Texture2D, mTextureId);
-				GL.TexImage2D(TextureTarget.Texture2D,
-						  0,
-                          mIsPNG ? PixelInternalFormat.Rgba : PixelInternalFormat.Rgb,
-						  image.Width,
-						  image.Height,
-						  0,
-						  mIsPNG ? OpenTK.Graphics.OpenGL.PixelFormat.Rgba : OpenTK.Graphics.OpenGL.PixelFormat.Rgb,
-						  PixelType.UnsignedByte,
-						  image.Data);
-                
+				
+                if (gammaCorrection)
+                {
+					//glTexImage2D(GL_TEXTURE_2D, 0, gammaCorrection ? GL_SRGB : GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+					GL.TexImage2D(TextureTarget.Texture2D,
+								  0,
+                                  isPng ? PixelInternalFormat.SrgbAlpha : PixelInternalFormat.Srgb,
+								  image.Width,
+								  image.Height,
+								  0,
+                                  isPng ? OpenTK.Graphics.OpenGL.PixelFormat.Rgba : OpenTK.Graphics.OpenGL.PixelFormat.Rgb,
+								  PixelType.UnsignedByte,
+								  image.Data);
+                }
+                else 
+                {
+					GL.TexImage2D(TextureTarget.Texture2D,
+								  0,
+								  isPng ? PixelInternalFormat.Rgba : PixelInternalFormat.Rgb,
+								  image.Width,
+								  image.Height,
+								  0,
+								  isPng ? OpenTK.Graphics.OpenGL.PixelFormat.Rgba : OpenTK.Graphics.OpenGL.PixelFormat.Rgb,
+								  PixelType.UnsignedByte,
+								  image.Data);
+                }
+
 				GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
                 GL.TexParameter(TextureTarget.Texture2D,
                                 TextureParameterName.TextureWrapS,
-                                mRepeatOrClampToEdge ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
+                                repeatOrClampToEdge ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
                 
                 GL.TexParameter(TextureTarget.Texture2D, 
                                 TextureParameterName.TextureWrapT, 
-                                mRepeatOrClampToEdge ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
+                                repeatOrClampToEdge ? (int)TextureWrapMode.Repeat : (int)TextureWrapMode.ClampToEdge);
                 
                 GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMagFilter.Nearest);
 				GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
@@ -63,7 +78,5 @@ namespace YH
 		}
 
 		private int mTextureId = 0;
-        private bool mIsPNG = false;
-        private bool mRepeatOrClampToEdge = true;
 	}
 }
