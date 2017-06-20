7f2c824d87b27098c305355c381925ba28c3a4de
yh <yanghang@egret.com>
2017-06-20 13:42:01 +0800

    简单整理

--- Projects/YH/YH/demo/HelloAntiAliasing.cs
+++ Projects/YH/YH/demo/HelloAntiAliasing.cs
@@ -20,20 +20,12 @@ namespace YH
 
 			mCamera = new Camera(new Vector3(0.0f, 0.0f, 5.0f), new Vector3(0.0f, 1.0f, 0.0f), Camera.YAW, Camera.PITCH);
 			mCameraController = new CameraController(mAppName, mCamera);
+			
+            mLightShader = new GLProgram(@"Resources/colors.vs", @"Resources/colors.frag");
 
-			//
-			mLightShader = new GLProgram(@"Resources/colors.vs", @"Resources/colors.frag");
-			mLocLightModel = mLightShader.GetUniformLocation("model");
-			mLocLightView = mLightShader.GetUniformLocation("view");
-			mLocLightProjection = mLightShader.GetUniformLocation("projection");
-			mLocLightObjectColor = mLightShader.GetUniformLocation("objectColor");
-			mLocLightColor = mLightShader.GetUniformLocation("lightColor");
-
-			//
-			mLampShader = new GLProgram(@"Resources/lamp.vs", @"Resources/lamp.frag");
-			mLocLampModel = mLampShader.GetUniformLocation("model");
-			mLocLampView = mLampShader.GetUniformLocation("view");
-			mLocLampProjection = mLampShader.GetUniformLocation("projection");
+			GL.Viewport(0, 0, wnd.Width, wnd.Height);
+			GL.ClearColor(Color.Gray);
+            GL.Enable(EnableCap.DepthTest);
 		}
 
 		public override void Update(double dt)
@@ -43,69 +35,30 @@ namespace YH
 
 		public override void Draw(double dt, Window wnd)
 		{
-			GL.Viewport(0, 0, wnd.Width, wnd.Height);
-			GL.ClearColor(Color.Gray);
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			GL.Enable(EnableCap.DepthTest);
-
-			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), (float)wnd.Width / (float)wnd.Height, 0.1f, 100.0f);
+			
+            var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom),
+                                                                  (float)wnd.Width / (float)wnd.Height, 
+                                                                  0.1f, 100.0f);
 			var view = mCamera.GetViewMatrix();
 
-			do
-			{
-				mLightShader.Use();
+			mLightShader.Use();
 
-				GL.UniformMatrix4(mLocLightProjection, false, ref projection);
-				GL.UniformMatrix4(mLocLightView, false, ref view);
+			GL.UniformMatrix4(mLightShader.GetUniformLocation("projection"), false, ref projection);
+			GL.UniformMatrix4(mLightShader.GetUniformLocation("view"), false, ref view);
 
-				GL.Uniform3(mLocLightObjectColor, 1.0f, 0.5f, 0.31f);
-				GL.Uniform3(mLocLightColor, 1.0f, 0.5f, 1.0f);
+			GL.Uniform3(mLightShader.GetUniformLocation("objectColor"), 1.0f, 0.5f, 0.31f);
+			GL.Uniform3(mLightShader.GetUniformLocation("lightColor"), 1.0f, 0.5f, 1.0f);
 
-				Matrix4 model = Matrix4.CreateTranslation(0, 0, 0);
-				model = Matrix4.CreateScale(0.5f) * model;
-				GL.UniformMatrix4(mLocLightModel, false, ref model);
+			Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.0f, 0.0f);
+			GL.UniformMatrix4(mLightShader.GetUniformLocation("model"), false, ref model);
 
-				mCube.Draw();
-
-			}
-			while (false);
-
-			do
-			{
-				mLampShader.Use();
-
-				GL.UniformMatrix4(mLocLampProjection, false, ref projection);
-				GL.UniformMatrix4(mLocLampView, false, ref view);
-
-				Matrix4 model = Matrix4.CreateTranslation(1.2f, 1.0f, 2.0f);
-				model = Matrix4.CreateScale(0.2f) * model;
-				GL.UniformMatrix4(mLocLampModel, false, ref model);
-
-				mSphere.Draw();//mCube.Draw();
-
-			}
-			while (false);
+			mCube.Draw();
 		}
 
 		private Cube mCube = null;
 		private Sphere mSphere = null;
-
 		private Camera mCamera = null;
-
-		//
 		private GLProgram mLightShader = null;
-		private int mLocLightModel = -1;
-		private int mLocLightView = -1;
-		private int mLocLightProjection = -1;
-		private int mLocLightObjectColor = -1;
-		private int mLocLightColor = -1;
-
-		//
-		private GLProgram mLampShader = null;
-		private int mLocLampModel = -1;
-		private int mLocLampView = -1;
-		private int mLocLampProjection = -1;
 	}
-
-
 }
