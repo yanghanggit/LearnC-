43f9143bd59a9a54beaa4a25b6b75512d7eaaa7a
yh <yanghang@egret.com>
2017-06-21 14:01:40 +0800

    简单绘制场景

--- Projects/YH/YH/demo/HelloShadowMapping.cs
+++ Projects/YH/YH/demo/HelloShadowMapping.cs
@@ -22,17 +22,11 @@ namespace YH
 			mCamera = new Camera(new Vector3(0.0f, 0.0f, 5.0f), new Vector3(0.0f, 1.0f, 0.0f), Camera.YAW, Camera.PITCH);
 			mCameraController = new CameraController(mAppName, mCamera);
 			mShader = new GLProgram(@"Resources/advanced.vs", @"Resources/advanced.frag");
-			mCubeTexture = new GLTexture2D(@"Resources/Texture/wall.jpg");
+			mCubeTexture = new GLTexture2D(@"Resources/Texture/wood.png");
 			mFloorTexture = new GLTexture2D(@"Resources/Texture/metal.png");
-
-			//
-			//mDepthFunction.Add(DepthFunction.Less);
-			//mDepthFunction.Add(DepthFunction.Always);
-			//mDepthFunction.Add(DepthFunction.Never);
 
 			GL.Enable(EnableCap.DepthTest);
 			GL.DepthFunc(DepthFunction.Less);
-			
 			GL.ClearColor(Color.Gray);
 		}
 
@@ -44,11 +38,22 @@ namespace YH
 		public override void Draw(double dt, Window wnd)
 		{
 			GL.Viewport(0, 0, wnd.Width, wnd.Height);
+
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom), (float)wnd.Width / (float)wnd.Height, 0.1f, 100.0f);
+			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom),
+                                                                  (float)wnd.Width / (float)wnd.Height,
+                                                                  0.1f, 100.0f);
+            
 			var view = mCamera.GetViewMatrix();
 
+            mShader.Use();
+
+			GL.UniformMatrix4(mShader.GetUniformLocation("projection"), false, ref projection);
+			GL.UniformMatrix4(mShader.GetUniformLocation("view"), false, ref view);
+            RenderScene(mShader);
+
+            /*
 			mShader.Use();
 
 			GL.BindTexture(TextureTarget.Texture2D, mCubeTexture.getTextureId());
@@ -70,30 +75,41 @@ namespace YH
 			model = Matrix4.CreateTranslation(0.0f, -0.5f, 0.0f);
 			GL.UniformMatrix4(mShader.GetUniformLocation("model"), false, ref model);
 			mFloor.Draw();
+			*/
 		}
 
 		public override void OnKeyUp(OpenTK.Input.KeyboardKeyEventArgs e)
 		{
 			base.OnKeyUp(e);
-			//if (e.Key == OpenTK.Input.Key.Plus)
-			//{
-			//	++mDepthFuncIndex;
-			//	mDepthFuncIndex = mDepthFuncIndex >= mDepthFunction.Count ? 0 : mDepthFuncIndex;
-			//}
-			//else if (e.Key == OpenTK.Input.Key.Minus)
-			//{
-			//	--mDepthFuncIndex;
-			//	mDepthFuncIndex = mDepthFuncIndex < 0 ? 0 : mDepthFuncIndex;
-			//}
-			//else if (e.Key == OpenTK.Input.Key.C)
-			//{
-			//	mUseDepthTest = !mUseDepthTest;
-			//}
-			//else if (e.Key == OpenTK.Input.Key.Space)
-			//{
-			//	mUseDepthTest = true;
-			//	mDepthFuncIndex = 0;
-			//}
+		}
+
+        void RenderScene(GLProgram shader)
+		{
+            Matrix4 model = Matrix4.CreateTranslation(0, 0, 0);
+
+            GL.BindTexture(TextureTarget.Texture2D, mFloorTexture.getTextureId());
+
+            model = Matrix4.CreateTranslation(0.0f, -0.5f, 0.0f);
+			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
+            mFloor.Draw();
+
+            GL.BindTexture(TextureTarget.Texture2D, mCubeTexture.getTextureId());
+
+			model = Matrix4.CreateTranslation(0.0f, 1.5f, 0.0f);
+			model = Matrix4.CreateScale(0.5f) * model;
+			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
+            mCube.Draw();
+
+			model = Matrix4.CreateTranslation(2.0f, 0.0f, 1.0f);
+			model = Matrix4.CreateScale(0.5f) * model;
+			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
+			mCube.Draw();
+
+			model = Matrix4.CreateTranslation(-1.0f, 0.3f, 2.0f);
+            model = Matrix4.CreateFromAxisAngle(new Vector3(1.0f, 0.0f, 1.0f), 60.0f) * model;
+            model = Matrix4.CreateScale(0.5f) * model;
+			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
+			mCube.Draw();
 		}
 
 		private Cube mCube = null;
@@ -102,8 +118,5 @@ namespace YH
 		private GLProgram mShader = null;
 		private GLTexture2D mCubeTexture = null;
 		private GLTexture2D mFloorTexture = null;
-		//private bool mUseDepthTest = true;
-		//private int mDepthFuncIndex = 0;
-		//private List<DepthFunction> mDepthFunction = new List<DepthFunction>();
 	}
 }
