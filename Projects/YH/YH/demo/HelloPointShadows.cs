dc97c1add440dc076dac148bddc56b3903c86e88
yh <yanghang@egret.com>
2017-06-28 18:09:53 +0800

    提交代码

--- Projects/YH/YH/demo/HelloPointShadows.cs
+++ Projects/YH/YH/demo/HelloPointShadows.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿using System;
+﻿﻿﻿﻿﻿using System;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using OpenTK;
@@ -23,18 +23,6 @@ namespace YH
 			mCamera = new Camera(new Vector3(0.0f, 0.0f, 5.0f), new Vector3(0.0f, 1.0f, 0.0f), Camera.YAW, Camera.PITCH);
 			mCameraController = new CameraController(mAppName, mCamera);
 
-			mShader = new GLProgram(@"Resources/cubemaps.vs", @"Resources/cubemaps.frag");
-			mSkyboxShader = new GLProgram(@"Resources/skybox.vs", @"Resources/skybox.frag");
-
-			mGLTextureCube = new GLTextureCube(
-				@"Resources/Texture/skybox/right.jpg",
-				@"Resources/Texture/skybox/left.jpg",
-				@"Resources/Texture/skybox/top.jpg",
-				@"Resources/Texture/skybox/bottom.jpg",
-				@"Resources/Texture/skybox/back.jpg",
-				@"Resources/Texture/skybox/front.jpg"
-			);
-
             //
             mGLDepthMapFramebuffer = new GLDepthMapFramebuffer(1024, 1024, 
                                                                new Vector4(1, 1, 1, 1),
@@ -43,7 +31,16 @@ namespace YH
             mShadowTransforms = BuildShadowTransforms(mLightPos, 1024, 1024);
 
             simpleDepthShader = new GLProgram(@"Resources/point_shadows_depth.vs", @"Resources/point_shadows_depth.frag", @"Resources/point_shadows_depth.gs");
-            //Shader simpleDepthShader("point_shadows_depth.vs", "point_shadows_depth.frag", "point_shadows_depth.gs");
+			shader = new GLProgram(@"Resources/point_shadows.vs", @"Resources/point_shadows.frag");
+            woodTexture = new GLTexture2D(@"Resources/Texture/wood.png");
+
+            shader.Use();
+            GL.Uniform1(shader.GetUniformLocation("diffuseTexture"), 0);
+            GL.Uniform1(shader.GetUniformLocation("depthMap"), 1);
+
+			GL.Enable(EnableCap.DepthTest);
+			GL.DepthFunc(DepthFunction.Less);
+            GL.ClearColor(Color.Gray);
 		}
 
 		public override void Update(double dt)
@@ -68,8 +65,8 @@ namespace YH
 			}
 			
             const float far = 25.0f;
-			GL.Uniform1(mShader.GetUniformLocation("far_plane"), far);
-            GL.Uniform3(mShader.GetUniformLocation("lightPos"), mLightPos);
+			GL.Uniform1(simpleDepthShader.GetUniformLocation("far_plane"), far);
+            GL.Uniform3(simpleDepthShader.GetUniformLocation("lightPos"), mLightPos);
 			RenderScene(simpleDepthShader);
             GL.BindFramebuffer(FramebufferTarget.Framebuffer, 0);
         }
@@ -79,38 +76,30 @@ namespace YH
             DrawDepthMap(dt, wnd);
 
 			GL.Viewport(0, 0, wnd.Width, wnd.Height);
-			GL.ClearColor(Color.Gray);
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-			GL.Enable(EnableCap.DepthTest);
-			GL.DepthFunc(DepthFunction.Less);
-
-			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom), (float)wnd.Width / (float)wnd.Height, 0.1f, 100.0f);
+			shader.Use();
+			var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom),
+                                                                  (float)wnd.Width / (float)wnd.Height,
+                                                                  0.1f, 100.0f);
 			var view = mCamera.GetViewMatrix();
 			Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.0f, 0.0f);
 
-			// Draw scene as normal
-			mShader.Use();
+			GL.UniformMatrix4(shader.GetUniformLocation("projection"), false, ref projection);
+			GL.UniformMatrix4(shader.GetUniformLocation("view"), false, ref view);
+			GL.Uniform3(shader.GetUniformLocation("lightPos"), mLightPos);
+            GL.Uniform3(shader.GetUniformLocation("viewPos"), mCamera.Position);
 
-			GL.BindTexture(TextureTarget.TextureCubeMap, mGLTextureCube.mTextureCubeId);
-			GL.Uniform1(mShader.GetUniformLocation("ratio"), ParseRatio(mRatioIndex));
+			const float far = 25.0f;
+			GL.Uniform1(shader.GetUniformLocation("far_plane"), far);
+			GL.Uniform1(shader.GetUniformLocation("shadows"), 1);
 
-			GL.UniformMatrix4(mShader.GetUniformLocation("projection"), false, ref projection);
-			GL.UniformMatrix4(mShader.GetUniformLocation("view"), false, ref view);
-			GL.Uniform3(mShader.GetUniformLocation("cameraPos"), mCamera.Position);
-
-            RenderScene(mShader);
-
-			// Draw skybox as last
-			GL.DepthFunc(DepthFunction.Equal);
-			mSkyboxShader.Use();
-			var skyView = new Matrix4(new Matrix3(view));
-			GL.UniformMatrix4(mSkyboxShader.GetUniformLocation("view"), false, ref skyView);
-			GL.UniformMatrix4(mSkyboxShader.GetUniformLocation("projection"), false, ref projection);
-			//GL.BindTexture(TextureTarget.TextureCubeMap, mGLTextureCube.mTextureCubeId);
+            GL.ActiveTexture(TextureUnit.Texture0);
+            GL.BindTexture(TextureTarget.Texture2D, woodTexture.getTextureId());
+			GL.ActiveTexture(TextureUnit.Texture1);
             GL.BindTexture(TextureTarget.TextureCubeMap, mGLDepthMapFramebuffer.mDepthMap);
-			mSkybox.Draw();
-			GL.DepthFunc(DepthFunction.Less);
+
+			RenderScene(shader);
 		}
 
         List<Matrix4> BuildShadowTransforms(Vector3 lightPos, int shadowWidth, int shadowHeight)
@@ -139,7 +128,7 @@ namespace YH
 
         void RenderScene(GLProgram shader)
         {
-            Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.0f, 0.0f);
+            Matrix4 model = Matrix4.CreateTranslation(0.0f, 0.5f, 0.0f);
 			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
 			mCube.Draw();
         }
@@ -149,44 +138,26 @@ namespace YH
 			base.OnKeyUp(e);
 			if (e.Key == OpenTK.Input.Key.Plus)
 			{
-				++mRatioIndex;
+
 			}
 			else if (e.Key == OpenTK.Input.Key.Minus)
 			{
-				--mRatioIndex;
-				mRatioIndex = mRatioIndex >= 0 ? mRatioIndex : 0;
+				
 			}
 			else if (e.Key == OpenTK.Input.Key.C)
 			{
 			}
 		}
 
-		float ParseRatio(int index)
-		{
-			float[] ratios = {
-				0.0f, //nothing
-                //1.00f,//空气
-                1.33f,//水
-                1.309f,//冰
-                1.52f,//玻璃
-                2.42f,//宝石
-            };
-
-			int mod = index % ratios.Length;
-			return ratios[mod];
-		}
-
 		private Cube mCube = null;
 		private Sphere mSphere = null;
 		private Camera mCamera = null;
-		private GLProgram mShader = null;
-		private GLProgram mSkyboxShader = null;
 		private Skybox mSkybox = null;
-		private GLTextureCube mGLTextureCube = null;
-		private int mRatioIndex = 0;
         private List<Matrix4> mShadowTransforms = new List<Matrix4>();
         private GLDepthMapFramebuffer mGLDepthMapFramebuffer = null;
         private Vector3 mLightPos = new Vector3(0, 0, 0);
         private GLProgram simpleDepthShader = null;
+		private GLProgram shader = null;
+		private GLTexture2D woodTexture = null;
 	}
 }
