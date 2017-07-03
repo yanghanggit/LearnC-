92dd444f4eafde79f7e93283c75deb8119248b45
yh <yanghang@egret.com>
2017-07-03 14:29:50 +0800

    修改变量的名字

--- Projects/YH/YH/demo/HelloHDR.cs
+++ Projects/YH/YH/demo/HelloHDR.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿﻿using System;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using OpenTK;
@@ -27,26 +27,26 @@ namespace YH
 			mCameraController = new CameraController(mAppName, mCamera);
 
             //
-            hdrFBO = new GLHDRFramebuffer(wnd.Width, wnd.Height);
-
-            //
-			shader = new GLProgram(@"Resources/lighting.vs", @"Resources/lighting.frag");
-			hdrShader = new GLProgram(@"Resources/hdr.vs", @"Resources/hdr.frag");
+            mHDRFBO = new GLHDRFramebuffer(wnd.Width, wnd.Height);
 
 			//
-			woodTexture = new GLTexture2D(@"Resources/Texture/wood.png");
+            mLightingShader = new GLProgram(@"Resources/lighting.vs", @"Resources/lighting.frag");
+			mHDRShader = new GLProgram(@"Resources/hdr.vs", @"Resources/hdr.frag");
+
+			//
+			mWoodTexture = new GLTexture2D(@"Resources/Texture/wood.png");
 
             //
-            lightPositions.Add(new Vector3(0.0f, 0.0f, 49.5f));
-			lightPositions.Add(new Vector3(-1.4f, -1.9f, 9.0f));
-			lightPositions.Add(new Vector3(0.0f, -1.8f, 4.0f));
-			lightPositions.Add(new Vector3(0.8f, -1.7f, 6.0f));
+            mLightPositions.Add(new Vector3(0.0f, 0.0f, 49.5f));
+			mLightPositions.Add(new Vector3(-1.4f, -1.9f, 9.0f));
+			mLightPositions.Add(new Vector3(0.0f, -1.8f, 4.0f));
+			mLightPositions.Add(new Vector3(0.8f, -1.7f, 6.0f));
 		
             //
-			lightColors.Add(new Vector3(200.0f, 200.0f, 200.0f));
-			lightColors.Add(new Vector3(0.1f, 0.0f, 0.0f));
-			lightColors.Add(new Vector3(0.0f, 0.0f, 0.2f));
-			lightColors.Add(new Vector3(0.0f, 0.1f, 0.0f));
+			mLightColors.Add(new Vector3(200.0f, 200.0f, 200.0f));
+			mLightColors.Add(new Vector3(0.1f, 0.0f, 0.0f));
+			mLightColors.Add(new Vector3(0.0f, 0.0f, 0.2f));
+			mLightColors.Add(new Vector3(0.0f, 0.1f, 0.0f));
 
             //
             GL.Viewport(0, 0, wnd.Width, wnd.Height);
@@ -61,7 +61,7 @@ namespace YH
 
 		public override void Draw(double dt, Window wnd)
 		{
-            GL.BindFramebuffer(FramebufferTarget.Framebuffer, hdrFBO.mHDRFBO);
+            GL.BindFramebuffer(FramebufferTarget.Framebuffer, mHDRFBO.mHDRFBO);
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
             var projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(mCamera.Zoom),
                                                                   (float)wnd.Width / (float)wnd.Height,
@@ -70,23 +70,23 @@ namespace YH
 
             var model = Matrix4.CreateTranslation(0, 0, 0);
 
-			shader.Use();
-			GL.UniformMatrix4(shader.GetUniformLocation("projection"), false, ref projection);
-			GL.UniformMatrix4(shader.GetUniformLocation("view"), false, ref view);
+			mLightingShader.Use();
+			GL.UniformMatrix4(mLightingShader.GetUniformLocation("projection"), false, ref projection);
+			GL.UniformMatrix4(mLightingShader.GetUniformLocation("view"), false, ref view);
 
             GL.ActiveTexture(TextureUnit.Texture0);
-            GL.BindTexture(TextureTarget.Texture2D, woodTexture.getTextureId());
+            GL.BindTexture(TextureTarget.Texture2D, mWoodTexture.getTextureId());
 
-            for (var i = 0; i < lightPositions.Count; ++i)
+            for (var i = 0; i < mLightPositions.Count; ++i)
 			{
-                GL.Uniform3(shader.GetUniformLocation("lights[" + i + "].Position"), lightPositions[i]);
-                GL.Uniform3(shader.GetUniformLocation("lights[" + i + "].Color"), lightColors[i]);
+                GL.Uniform3(mLightingShader.GetUniformLocation("lights[" + i + "].Position"), mLightPositions[i]);
+                GL.Uniform3(mLightingShader.GetUniformLocation("lights[" + i + "].Color"), mLightColors[i]);
 			}
-            GL.Uniform3(shader.GetUniformLocation("viewPos"), mCamera.Position);
+            GL.Uniform3(mLightingShader.GetUniformLocation("viewPos"), mCamera.Position);
             model = Matrix4.CreateTranslation(0.0f, 0.0f, 25.0f);
             model = Matrix4.CreateScale(5.0f, 5.0f, 55.0f) * model;
-			GL.UniformMatrix4(shader.GetUniformLocation("model"), false, ref model);
-			GL.Uniform1(shader.GetUniformLocation("inverse_normals"), 1);
+			GL.UniformMatrix4(mLightingShader.GetUniformLocation("model"), false, ref model);
+			GL.Uniform1(mLightingShader.GetUniformLocation("inverse_normals"), 1);
 			mCube.Draw();
 
 			GL.BindTexture(TextureTarget.Texture2D, 0);
@@ -95,12 +95,12 @@ namespace YH
 
 			//
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-			hdrShader.Use();
+			mHDRShader.Use();
 
 			GL.ActiveTexture(TextureUnit.Texture0);
-            GL.BindTexture(TextureTarget.Texture2D, hdrFBO.mColorBuffer);
-			GL.Uniform1(hdrShader.GetUniformLocation("hdr"), 1);
-			GL.Uniform1(hdrShader.GetUniformLocation("exposure"), mExposure);
+            GL.BindTexture(TextureTarget.Texture2D, mHDRFBO.mColorBuffer);
+			GL.Uniform1(mHDRShader.GetUniformLocation("hdr"), 1);
+			GL.Uniform1(mHDRShader.GetUniformLocation("exposure"), mExposure);
 			mQuad.Draw();
 		}
 
@@ -137,12 +137,12 @@ namespace YH
 		private Cube mCube = null;
         private Quad mQuad = null;
 		private Camera mCamera = null;
-        private GLHDRFramebuffer hdrFBO = null;
-        private GLProgram shader = null;
-        private GLProgram hdrShader = null;
-		private GLTexture2D woodTexture = null;
-        private List<Vector3> lightPositions = new List<Vector3>();
-        private List<Vector3> lightColors = new List<Vector3>();
+        private GLHDRFramebuffer mHDRFBO = null;
+        private GLProgram mLightingShader = null;
+        private GLProgram mHDRShader = null;
+		private GLTexture2D mWoodTexture = null;
+        private List<Vector3> mLightPositions = new List<Vector3>();
+        private List<Vector3> mLightColors = new List<Vector3>();
         private float mExposure = 1.0f;
 	}
 }
