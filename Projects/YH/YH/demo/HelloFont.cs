2df0cb4488be12084efbd8c46a32de40bf744934
yh <yanghang@egret.com>
2017-07-10 15:09:09 +0800

    添加一些代码

--- Projects/YH/YH/demo/HelloFont.cs
+++ Projects/YH/YH/demo/HelloFont.cs
@@ -136,19 +136,25 @@ namespace YH
             face.Dispose();
 			mLibrary.Dispose();
 
-
-
-			// Configure VAO/VBO for texture quads
-			//glGenVertexArrays(1, &VAO);
-			//glGenBuffers(1, &VBO);
-			//glBindVertexArray(VAO);
-			//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-			//glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
-			//glEnableVertexAttribArray(0);
+            // Configure VAO/VBO for texture quads
+            //glGenVertexArrays(1, &VAO);
+            VAO = GL.GenVertexArray();
+            //glGenBuffers(1, &VBO);
+            VBO = GL.GenBuffer();
+            //glBindVertexArray(VAO);
+            GL.BindVertexArray(VAO);
+            //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+            //glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(float) * 6 * 4, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+            //glEnableVertexAttribArray(0);
+            GL.EnableVertexAttribArray(0);
 			//glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+			GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 4 * sizeof(float), 0);
 			//glBindBuffer(GL_ARRAY_BUFFER, 0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 			//glBindVertexArray(0);
-
+			GL.BindVertexArray(0);
 		}
 
 		public override void Update(double dt)
@@ -159,12 +165,72 @@ namespace YH
 		public override void Draw(double dt, Window wnd)
 		{
 			GL.Clear(ClearBufferMask.ColorBufferBit);
+			// Clear the colorbuffer
+			//glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+			//glClear(GL_COLOR_BUFFER_BIT);
+            RenderText(shader, "This is sample text", 25.0f, 25.0f, 1.0f, new Vector3(0.5f, 0.8f, 0.2f));
+			RenderText(shader, "(C) LearnOpenGL.com", 540.0f, 570.0f, 0.5f, new Vector3(0.3f, 0.7f, 0.9f));
 		}
 
 		public override void OnKeyUp(OpenTK.Input.KeyboardKeyEventArgs e)
 		{
 			base.OnKeyUp(e);
 		}
+
+        public void RenderText(GLProgram sh, string text, float x, float y, float scale, Vector3 color)
+		{
+			// Activate corresponding render state  
+			sh.Use();
+			//glUniform3f(glGetUniformLocation(shader.Program, "textColor"), color.x, color.y, color.z);
+            GL.Uniform3(sh.GetUniformLocation("textColor"), color.X, color.Y, color.Z);
+			//glActiveTexture(GL_TEXTURE0);
+            GL.ActiveTexture(TextureUnit.Texture0);
+			//glBindVertexArray(VAO);
+            GL.BindVertexArray(VAO);
+
+            // Iterate through all characters
+            //std::string::const_iterator c;
+            //for (c = text.begin(); c != text.end(); c++)
+            foreach (var c in text)
+			{
+                Character ch = mCharacters[c];//Characters[c];
+                float xpos = x + ch.Bearing.X * scale;
+                float ypos = y - (ch.Size.Height - ch.Bearing.Y) * scale;
+                float w = ch.Size.Width * scale;
+                float h = ch.Size.Height * scale;
+				// Update VBO for each character
+				/*
+				GLfloat vertices[6][4] = {
+		            { xpos,     ypos + h,   0.0, 0.0 },            
+		            { xpos,     ypos,       0.0, 1.0 },
+		            { xpos + w, ypos,       1.0, 1.0 },
+
+		            { xpos,     ypos + h,   0.0, 0.0 },
+		            { xpos + w, ypos,       1.0, 1.0 },
+		            { xpos + w, ypos + h,   1.0, 0.0 }           
+		        };
+
+				// Render glyph texture over quad
+				glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+				// Update content of VBO memory
+				glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+				glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+
+				glBindBuffer(GL_ARRAY_BUFFER, 0);
+				// Render quad
+				glDrawArrays(GL_TRIANGLES, 0, 6);
+				// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+				x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+                */
+			}
+            //glBindVertexArray(0);
+            GL.BindVertexArray(0);
+			//glBindTexture(GL_TEXTURE_2D, 0);
+            GL.BindTexture(TextureTarget.Texture2D, 0);
+		}
+
 
         private Library mLibrary = null;
         private Dictionary<char, Character> mCharacters = new Dictionary<char, Character>();
