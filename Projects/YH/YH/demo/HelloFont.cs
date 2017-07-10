c57b205004bb0cd85445ddd67c7b37d62f2900d0
yh <yanghang@egret.com>
2017-07-10 16:29:00 +0800

    临时传一下，初步完成。

--- Projects/YH/YH/demo/HelloFont.cs
+++ Projects/YH/YH/demo/HelloFont.cs
@@ -199,38 +199,47 @@ namespace YH
                 float w = ch.Size.Width * scale;
                 float h = ch.Size.Height * scale;
 				// Update VBO for each character
-				/*
-				GLfloat vertices[6][4] = {
-		            { xpos,     ypos + h,   0.0, 0.0 },            
-		            { xpos,     ypos,       0.0, 1.0 },
-		            { xpos + w, ypos,       1.0, 1.0 },
+				
+				//GLfloat vertices[6][4] = {
+		        //    { xpos,     ypos + h,   0.0, 0.0 },            
+		        //    { xpos,     ypos,       0.0, 1.0 },
+		        //    { xpos + w, ypos,       1.0, 1.0 },
 
-		            { xpos,     ypos + h,   0.0, 0.0 },
-		            { xpos + w, ypos,       1.0, 1.0 },
-		            { xpos + w, ypos + h,   1.0, 0.0 }           
-		        };
+		        //    { xpos,     ypos + h,   0.0, 0.0 },
+		        //    { xpos + w, ypos,       1.0, 1.0 },
+		        //    { xpos + w, ypos + h,   1.0, 0.0 }           
+		        //};
+                float[] vertices = {
+                     xpos,     ypos + h,   0.0f, 0.0f,
+                     xpos,     ypos,       0.0f, 1.0f,
+                     xpos + w, ypos,       1.0f, 1.0f,
+
+                     xpos,     ypos + h,   0.0f, 0.0f,
+                     xpos + w, ypos,       1.0f, 1.0f,
+                     xpos + w, ypos + h,   1.0f, 0.0f
+				};
 
 				// Render glyph texture over quad
-				glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+				//glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+                GL.BindTexture(TextureTarget.Texture2D, ch.TextureID);
 				// Update content of VBO memory
-				glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-				glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
-
-
-				glBindBuffer(GL_ARRAY_BUFFER, 0);
+				//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+                //glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+                GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, sizeof(float) * vertices.Length, vertices);
+				//glBindBuffer(GL_ARRAY_BUFFER, 0);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
 				// Render quad
-				glDrawArrays(GL_TRIANGLES, 0, 6);
+				//glDrawArrays(GL_TRIANGLES, 0, 6);
+                GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
 				// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
 				x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
-                */
 			}
             //glBindVertexArray(0);
             GL.BindVertexArray(0);
 			//glBindTexture(GL_TEXTURE_2D, 0);
             GL.BindTexture(TextureTarget.Texture2D, 0);
 		}
-
 
         private Library mLibrary = null;
         private Dictionary<char, Character> mCharacters = new Dictionary<char, Character>();
